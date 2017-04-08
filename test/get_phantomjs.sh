afb95888220dd0d0a15d5013c99ba02fdd9fa127
INARES <contact@inares.org>
2017-04-08 11:58:38 +0000

    Modifications

--- test/get_phantomjs.sh
+++ test/get_phantomjs.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo "Downloading PhantomJS ..."
 curl -o- -sSL https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 | tar --strip-components=2 -xjf - phantomjs-2.1.1-linux-x86_64/bin/phantomjs
 sudo ln -sf `pwd`/phantomjs /usr/local/bin/phantomjs
+ll `pwd`/phantomjs
