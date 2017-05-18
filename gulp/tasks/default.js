8bf6ba064230c51f3dd4fb8d2002b591236079d9
INARES <contact@inares.org>
2017-05-18 15:29:38 +0000

    Remove CSS source map for production

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -16,6 +16,7 @@ gulp.task('default', ['build']);
 
 gulp.task("travis", function(done) {
   const sequence     = require("run-sequence");
+  const isProduction = require("../util/getArgs").isProduction;
 
   sequence("sass", "jekyll-travis", "javascript", "copy-validation", "rev:collect", "lint-css", done);
 });
